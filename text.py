ee4cfdb3c40b299bf6ddf8aecd60a9b676a2036f
Atomk <marco.fras@hotmail.it>
2025-04-03 20:49:20 +0200

    minor restructuring

--- text.py
+++ text.py
@@ -8,13 +8,13 @@ font = pygame.font.Font(default_font, 32)
 #font = pygame.font.SysFont("Arial", 32)
 
 class Text:
-    def __init__(self, string):
-        self.color = "white"
+    def __init__(self, string: str, color="white"):
+        self.color = color
         self.update_string(string)
         self.rect = self.texture.get_rect()
 
     def update_string(self, string: str):
         self.texture = font.render(string, True, self.color)
 
-    def draw(self, screen):
+    def draw(self, screen: pygame.Surface):
         screen.blit(self.texture, self.rect)
