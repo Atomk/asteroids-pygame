e7a68d96835ea01b20b621c0b6aa46fdfd9f3d77
Atomk <marco.fras@hotmail.it>
2025-04-04 23:55:11 +0200

    feat: shots wraparound

--- shot.py
+++ shot.py
@@ -5,9 +5,26 @@ from constants import *
 class Shot(CircleShape):
     def __init__(self, x, y, radius):
         super().__init__(x, y, radius)
+        self.lifetime = 0
 
     def update(self, delta_time):
+        # Lifeitime prevents infinite wraparound
+        self.lifetime += delta_time
+        if self.lifetime > SHOT_LIFETIME:
+            self.kill()
+            return
+
         self.position += self.velocity * delta_time
+        # wraparound x
+        if self.position.x < -self.radius:
+            self.position.x = SCREEN_WIDTH + self.radius
+        elif self.position.x > SCREEN_WIDTH + self.radius:
+            self.position.x = -self.radius
+        # wraparound y
+        if self.position.y < -self.radius:
+            self.position.y = SCREEN_HEIGHT + self.radius
+        elif self.position.y > SCREEN_HEIGHT + self.radius:
+            self.position.y = -self.radius
 
     def draw(self, screen):
-        pygame.draw.circle(screen, "white", self.position, self.radius)
+        pygame.draw.circle(screen, "white", self.position, self.radius, 1)
